18a84c8999a014f6db7ec4e461864f1ecaab3873
longhoang <long542.nt@gmail.com>
2025-02-07 01:52:56 +0700

    bai18_7-2-2025

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/hoidanit', (req, res) => {
+    // res.send('<h1>long dep trai</h1>')
+    res.render('sample.ejs');
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
