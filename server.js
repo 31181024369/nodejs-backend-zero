34aee6bdff69938bda6254539fb6ea076e67ffc4
longhoang <long542.nt@gmail.com>
2025-02-07 01:18:49 +0700

    bai17_7-2-2025

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+app.get('/hoidanit', (req, res) => {
+    res.send('<h1>long dep trai</h1>')
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
