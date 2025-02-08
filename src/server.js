826634f66a4e74c82e07095acb85a90a5b0b3e2b
longhoang <long542.nt@gmail.com>
2025-02-08 19:43:39 +0700

    8-2-2025

--- src/server.js
+++ src/server.js
@@ -5,11 +5,27 @@ const webRoutes = require('./routes/web');
 const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
-
+const mysql = require('mysql2');
 configViewEngine(app);
 
 
 app.use('/v1', webRoutes);
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '123456',
+    database: 'hoidanit',
+});
+
+connection.query(
+    'SELECT * FROM `Users` u',
+    function (err, results, fields) {
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+    }
+)
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
